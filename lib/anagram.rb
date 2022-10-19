7c2f0384c6b68d6ab6f2c3b985a8eeebbbb2252f
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-19 08:42:55 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(inputs)
+    inputs.select do |i|
+        i.split("").sort == word.split("").sort
+    end
+    end
+end
